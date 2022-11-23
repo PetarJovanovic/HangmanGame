a61b19c9960b51f0d9bd2e291b0f906f8e25526f
Petar Jovanovic <pecazajecar@gmail.com>
2022-11-23 20:48:38 +0100

    Deleted testing file

--- js/test.js
+++
@@ -1,8 +0,0 @@
-const input = document.getElementById('input');
-input.addEventListener('keypress', function (e) {
-  if (e.key === 'Enter') {
-    e.preventDefault();
-    document.getElementById('result').innerHTML = input.value;
-    document.getElementById('input').value = '';
-  }
-});
